783cb362c168688d56ecc9fedcd36bfcd229cbc7
Pushkar Borkar <pushkarborkar1809@gmail.com>
2021-10-29 03:29:37 +0530

    chatbot ui set up done

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import joy1 from "../assets/joy1.png";
 import joy2 from "../assets/joy2.png";
 import joy3 from "../assets/joy3.png";
 import joy4 from "../assets/joy4.png";
 import joy5 from "../assets/joy5.png";
+import Nav from "./Nav";
+import "../styles/chat.scss";
+import { RiSendPlane2Fill as Send } from "react-icons/ri";
 
 const Chat = () => {
 	const joys = [joy1, joy2, joy3, joy4, joy5];
 	let joy = joys[Math.floor(Math.random() * joys.length)];
 
+	const [message, setMessage] = useState("");
+	const handleMessage = (e) => {
+		setMessage(e.target.value);
+	};
+
 	return (
-		<div className="chat">
-			<div className="joy">
-				<img src={joy} alt="Joy: The ChatBot" style={{ width: "10rem" }} />
+		<>
+			<Nav />
+			<div className="chat">
+				<div className="chatbox">
+					<div className="box">
+						<div className="input-area">
+							<input
+								type="text"
+								id="message"
+								name="message"
+								onChange={(e) => handleMessage(e)}
+							/>
+							<button>
+								<Send size={30} />
+							</button>
+						</div>
+						<div className="chat-display"></div>
+					</div>
+				</div>
+				<div className="joy">
+					<img src={joy} alt="Joy: The ChatBot" />
+				</div>
 			</div>
-			<div className="chatbox">
-				<div className="chat-display"></div>
-				<input type="text" id="message" name="message" />
-				<button>send</button>
-			</div>
-		</div>
+		</>
 	);
 };
 
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,13 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BsFillChatSquareFill as ChatIcon } from "react-icons/bs";
+import { CgMediaPodcast as Media } from "react-icons/cg";
+import {
+	SiHandshake as HandshakeIcon,
+	SiRobotframework as Robo,
+} from "react-icons/si";
+import { GoPerson as ProfileIcon } from "react-icons/go";
+import "../styles/nav.scss";
 
 const Nav = () => {
 	return (
-		<div className="nav">
-			<div className="chat-icon"></div>
+		<nav className="navbar">
+			<ul className="navbar-nav">
+				<li className="nav-item logo-item">
+					<Link to="/app" className="logo">
+						<span className="logo-text">JOY</span>
+					</Link>
+				</li>
+				<li className="nav-item chat-item">
+					<Link to="/app" className="nav-link">
+						<ChatIcon size={40} />
+					</Link>
+				</li>
+				<li className="nav-item feed-item">
+					<Link to="/feed" className="nav-link">
+						<Media size={40} />
+					</Link>
+				</li>
+				<li className="nav-item psy-item">
+					<Link to="/psy" className="nav-link">
+						<HandshakeIcon size={40} />
+					</Link>
+				</li>
+				<li className="nav-item profile-item">
+					<Link to="#" className="nav-link">
+						<ProfileIcon size={40} />
+					</Link>
+				</li>
+			</ul>
+			{/* <div className="chat-icon"></div>
 			<div className="feed-icon"></div>
 			<div className="psy-icon"></div>
-			<div className="profile-icon"></div>
-		</div>
+			<div className="profile-icon"></div> */}
+		</nav>
 	);
 };
 
